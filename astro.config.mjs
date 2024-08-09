749443d5264fb35cf9d892d8cde4e269237b8be8
Alessandro <a.latella@relux.com>
2024-08-09 12:00:08 +0200

    [fix] site and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
-  integrations: [tailwind(), mdx(), sitemap(), icon()]
+  output: 'static',
+  site: 'https://northsouth-astro.vercel.app',
+  adapter: vercel(),
+  integrations: [tailwind(), mdx(), sitemap(), icon(),]
 });
